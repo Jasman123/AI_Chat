9f4967bc350b3731eef2bfe054bc164257ed7491
Jasman <53416589+Jasman123@users.noreply.github.com>
2025-11-18 14:47:59 +0700

    multiple chat and save point

--- googlegemini.py
+++ googlegemini.py
@@ -1,13 +1,93 @@
 from dotenv import load_dotenv
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import StateGraph, START, END, MessagesState
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, RemoveMessage
+from typing import Literal
 
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
 
-llm = ChatGoogleGenerativeAI(
+class State(MessagesState):
+    summary: str
+
+def ask_question(state: State) -> State:
+    
+    print(f"\n-------> ENTERING ask_question:")
+    
+    question = "What is your question?"
+    print(question)
+    
+    return State(messages = [AIMessage(question), HumanMessage(input())])
+
+def chatbot(state: State) -> State:
+    
+    print(f"\n-------> ENTERING chatbot:")
+    for i in state["messages"]:
+        i.pretty_print()
+        
+    system_message = f'''
+    Here's a quick summary of what's been discussed so far:
+    {state.get("summary", "")}
+    
+    Keep this in mind as you answer the next question.
+    '''
+    
+    response = chat.invoke([SystemMessage(system_message)] + state["messages"])
+    response.pretty_print()
+    
+    return State(messages = [response])
+
+def ask_another_question(state: State) -> State:
+    
+    print(f"\n-------> ENTERING ask_another_question:")
+    
+    question = "Would you like to ask one more question (yes/no)?"
+    print(question)
+    
+    return State(messages = [AIMessage(question), HumanMessage(input())])
+
+
+def summarize_and_delete_messages(state: State) -> State:
+    print(f"\n-------> ENTERING trim_messages:")
+    
+    new_conversation = ""
+    for i in state["messages"]:
+        new_conversation += f"{i.type}: {i.content}\n\n"
+        
+    summary_instructions = f'''
+    Update the ongoing summary by incorporating the new lines of conversation below.  
+    Build upon the previous summary rather than repeating it so that the result  
+    reflects the most recent context and developments.
+
+
+    Previous Summary:
+    {state.get("summary", "")}
+
+    New Conversation:
+    {new_conversation}
+    '''
+    
+    print(summary_instructions)
+    
+    summary = chat.invoke([HumanMessage(summary_instructions)])
+    
+    remove_messages = [RemoveMessage(id = i.id) for i in state["messages"][:]]
+    
+    return State(messages = remove_messages, summary = summary.content)
+
+
+def routing_function(state: State) -> Literal["summarize_and_delete_messages", "__end__"]:
+    
+    if state["messages"][-1].content == "yes":
+        return "summarize_and_delete_messages"
+    else:
+        return "__end__"
+
+
+chat = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0,
     max_tokens=None,
@@ -15,13 +95,22 @@ llm = ChatGoogleGenerativeAI(
     max_retries=2,
 )
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that suggests programming languages based on user preferences.",
-    ),
-    ("human", "I love Data Science and Machine Learning. Which programming language should I learn?"),
-]
 
-ai_msg = llm.invoke(messages)
-print(ai_msg.content)
+graph = StateGraph(State)
+
+graph.add_node("ask_question", ask_question)
+graph.add_node("chatbot", chatbot)
+graph.add_node("ask_another_question", ask_another_question)
+graph.add_node("summarize_and_delete_messages", summarize_and_delete_messages)
+
+graph.add_edge(START, "ask_question")
+graph.add_edge("ask_question", "chatbot")
+graph.add_edge("chatbot", "ask_another_question")
+graph.add_conditional_edges(source = "ask_another_question", 
+                            path = routing_function)
+graph.add_edge("summarize_and_delete_messages", "ask_question")
+
+
+graph_compiled = graph.compile()
+
+graph_compiled.invoke(State(messages = []))
